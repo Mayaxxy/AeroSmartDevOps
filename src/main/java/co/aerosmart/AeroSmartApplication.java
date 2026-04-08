8166580dd6221cfebdbe6f2ba626fe4e7cfdaeff
ElkinBG <elkinlol13@gmail.com>
2026-04-08 08:12:24 -0500

    Commit inicial

---
+++ src/main/java/co/aerosmart/AeroSmartApplication.java
@@ -0,0 +1,13 @@
+package co.aerosmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroSmartApplication.class, args);
+	}
+
+}
